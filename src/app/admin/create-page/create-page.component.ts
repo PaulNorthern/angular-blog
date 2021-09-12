a2a6c78baa731c1e13ee9962fe28a6507fc10fc8
PaulNorthern <sculltakers111@gmail.com>
2021-09-12 16:09:48 +0300

    message plugin

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Post} from "../../shared/interfaces";
 import {PostsService} from "../../shared/posts.service";
+import {AlertService} from "../shared/services/alert.service";
 
 @Component({
   selector: 'app-create-page',
@@ -13,7 +14,10 @@ export class CreatePageComponent implements OnInit {
   // @ts-ignore
   form: FormGroup;
 
-  constructor(private postsService: PostsService) { }
+  constructor(
+    private postsService: PostsService,
+    private alertService: AlertService
+    ) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -37,6 +41,7 @@ export class CreatePageComponent implements OnInit {
 
     this.postsService.create(post).subscribe(() => {
       this.form.reset()
+      this.alertService.success('Post was successful created!')
     })
   }
 
